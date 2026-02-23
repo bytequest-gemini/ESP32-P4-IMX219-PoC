9023f55e8cdcb5482ae6017435b740948c68c839
MAURO AGUJARI <mauroagujari@gmail.com>
2026-02-23 19:44:14 +0100

    Add files via upload

---
+++ main/jpeg_enc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "esp_err.h"
+#include <stdint.h>
+#include "driver/jpeg_encode.h"
+
+esp_err_t jpeg_enc_init(int width, int height);
+esp_err_t jpeg_enc_process(const void *src_data, int src_size, int width, int height, 
+                           uint32_t pixel_format, uint8_t **out_data, int *out_size);
+void jpeg_enc_free(uint8_t *data);
